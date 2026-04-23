ba106a65f16e41d77b4f4e98e2c89d146a82f94a
vyme001 <250090050+vyme001@users.noreply.github.com>
2026-04-23 10:14:13 -0400

    Edit the boilerplpate template logic from the initial project to accept the updated property names. Place it into the new posLogic function else condition

--- script.js
+++ script.js
@@ -1,60 +1,72 @@
 //playPositions
-currPositions = {
+const currPos = {
     user: undefined,
     pc: undefined,
+};
+
+//gameRules (key)winner:loser(value) pair
+const posRules = {
+    "rock":"scissors",
+    "paper":"rock",
+    "scissors":"paper",
 };
 
 
 //playerScore Board
 const scores = {
-    PCsScore : 0,
-    usersScore: 0,
-};
-
-
-//gameRules (key)winner:loser(value) pair
-const positionRules = {
-    "rock":"scissors",
-    "scissors":"paper",
-    "paper":"rock",
+    user: 0,
+    pc : 0,
 };
 
 
 //((PC position Generator
-function PCposition(){
-    const positions = Object.keys(positionRules);
-    currPositions.pc = positions[Math.floor(Math.random() * positions.length)];
+function PCpos(){
+    const positions = Object.keys(posRules);
+    currPos.pc = positions[Math.floor(Math.random() * positions.length)];
 };
+
 
 //executes score assignment for user and pc
-function scoreRunner(inn) {
-        currPositions.user = inn;
-        PCposition();
-        console.log(currPositions);
+function posAssignment(inn) {
+        currPos.user = inn;
+        PCpos();
 };
 
 
-//analyzes currentScores
-function analyzeScores(){
-    switch(currPositions.user){
-        case "rock":
-            currPositions.pc === "paper" ? console.log(`${currPositions.pc} beats ${currPositions.user}`) : currPositions.pc === "scissors" ? console.log(`${currPositions.user} beats ${currPositions.pc}`) : console.log('tie');
-        break;
-        case "paper":
-            currPositions.pc === "scissors" ? console.log(`${currPositions.pc} beats ${currPositions.user}`) : currPositions.pc === "rock" ? console.log(`${currPositions.user} beats ${currPositions.pc}`) : console.log('tie');
-        break;
-        case "scissors":
-            currPositions.pc === "rock" ? console.log(`${currPositions.pc} beats ${currPositions.user}`) : currPositions.pc === "paper" ? console.log(`${currPositions.user} beats ${currPositions.pc}`) : console.log('tie');
-        break;
-    }
-}
+//analyze currentScores #take1
+ /*
+       compare currPos to rulesObj by;
+       1). match inputs in order as  
+*/
+function posLogic(){
+    if(currPos.user === currPos.pc){
+        console.log('tie');
+        return;
+    } else{
+          Object.entries(posRules).find((posRules)=>{
+        if(posRules[0] === currPos.pc && posRules[1] === currPos.user){
+            scores.pc += 1;
+            console.log(`pc: (${currPos.pc}) beats ${currPos.user}. Scores: [pc: ${scores.pc}, user: ${scores.user}]`);
+            return;
+        } else if(posRules[0] === currPos.user && posRules[1] === currPos.pc){
+            scores.user += 1;
+            console.log(`user: (${currPos.user}) beats ${currPos.pc}. Scores: [user: ${scores.user}, pc: ${scores.pc}]`);
+            return;
+        };
+        return;
+    });
+         
+    };
+};
+
+
 
 //USER buttons trigger function
 const buttons = document.querySelectorAll("button");
 buttons.forEach((btn)=>{
     btn.addEventListener("click",(e)=>{
-        scoreRunner(e.target.value);
-        analyzeScores();
+        posAssignment(e.target.value);
+        posLogic();
         });
 })
 
@@ -70,11 +82,11 @@ const generateUserPosition = function(){
         });
     });
 
-    while(!Object.keys(positionRules).includes(humanPosition)){
+    while(!Object.keys(posRules).includes(humanPosition)){
 
         if(humanPosition === undefined){
             humanPosition = prompt("Choose one: 'rock', 'paper', or 'scissors'.").toLowerCase();
-        }else if(!Object.keys(positionRules).includes(humanPosition)){
+        }else if(!Object.keys(posRules).includes(humanPosition)){
         humanPosition = prompt(`'${humanPosition}' is not invalid, try again!`).toLowerCase();
         }else{
             return;
@@ -85,30 +97,20 @@ const generateUserPosition = function(){
 
 
 //Game logic engine
-function gameEngine(PCposition, UserPosition){
-    console.log(`cache (pc:[${PCposition}], user:[${UserPosition}])`);
+function gameEngine(currPos.pc, currPos.user){
+    console.log(`cache (pc:[${currPos.pc}], user:[${currPos.user}])`);
     
     //removed the if block from out of the find beacuase it executed for each item; 3 arrays = 3 occurances
-           if(PCposition === UserPosition){
-            alert(`Tie: ${PCposition} and ${UserPosition}`);
+           if(currPos.pc === currPos.user){
+            alert(`Tie: ${currPos.pc} and ${currPos.user}`);
             return;
         };
+};
+*/
+  
+    
 
-    Object.entries(positionRules).find((positionPair)=>{
-        if(positionPair[0] === PCposition && positionPair[1] === UserPosition){
-            PCsScore += 1;
-            alert(`pc: (${PCposition}) beats ${UserPosition}. Scores: [pc: ${PCsScore}, user: ${usersScore}]`);
-            return;
-        } else if(positionPair[0] === UserPosition && positionPair[1] === PCposition){
-            usersScore += 1;
-            alert(`user: (${UserPosition}) beats ${PCposition}. Scores: [user: ${usersScore}, pc: ${PCsScore}]`);
-            return;
-        };
-        return;
-    });
-    };
-
-
+/*
 //while no player has gained 5 points;
 // generate player position values
 // giving  those player positions to the gameEngine Fx
